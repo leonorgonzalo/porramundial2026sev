2c0bf921d93e5ca0e794c65f4b933912a0ca8962
leonorgonzalo <leonorgonzalof@gmail.com>
2026-05-30 18:45:15 +0200

    Update index.ts

--- db/index.ts
+++ db/index.ts
@@ -1,21 +1,7 @@
-import { drizzle } from "drizzle-orm/d1";
+import { drizzle } from "drizzle-orm/postgres-js";
+import postgres from "postgres";
 import * as schema from "./schema";
 
-let activeDb: any = null;
-
-// Esta función la llamará el index principal
-export function initDb(env: any) {
-  if (!activeDb) {
-    activeDb = drizzle(env.DB, { schema });
-  }
-}
-
-// Tus archivos de la carpeta /api seguirán usando esta 'db'
-export const db = new Proxy({}, {
-  get(target, prop) {
-    if (!activeDb) {
-      throw new Error("La base de datos D1 no ha sido inicializada todavía.");
-    }
-    return Reflect.get(activeDb, prop);
-  }
-}) as ReturnType<typeof drizzle<typeof schema>>;
+// Esto leerá el enlace de Supabase que acabas de guardar en Cloudflare
+const queryClient = postgres(process.env.DATABASE_URL!);
+export const db = drizzle(queryClient, { schema });
