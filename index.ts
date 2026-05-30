30eb2e4447428b04818572a26664ab4b287640b8
leonorgonzalo <leonorgonzalof@gmail.com>
2026-05-30 18:54:16 +0200

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,12 +1,6 @@
-import { initDb } from "./db/index";
-
 export default {
   async fetch(request: Request, env: any, ctx: any) {
-    // Inicializamos la conexión global a tu Postgres de Supabase
-    initDb(env);
-
-    // Si la petición viene de la web, dejamos que el motor de Cloudflare Assets
-    // sirva la interfaz y gestione las llamadas internas usando la DB ya inicializada
+    // Servimos directamente los archivos visuales y las rutas internas de la porra
     if (env.ASSETS) {
       return await env.ASSETS.fetch(request);
     }
