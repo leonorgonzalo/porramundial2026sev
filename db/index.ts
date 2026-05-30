6004d53b92685074897e55da1f0ffe2965a849f8
leonorgonzalo <leonorgonzalof@gmail.com>
2026-05-30 18:46:21 +0200

    Update index.ts

--- db/index.ts
+++ db/index.ts
@@ -1,7 +1,7 @@
-import { drizzle } from "drizzle-orm/postgres-js";
-import postgres from "postgres";
+import { drizzle } from "drizzle-orm/neon-http";
+import { neon } from "@neondatabase/serverless";
 import * as schema from "./schema";
 
-// Esto leerá el enlace de Supabase que acabas de guardar en Cloudflare
-const queryClient = postgres(process.env.DATABASE_URL!);
-export const db = drizzle(queryClient, { schema });
+// Conectamos a Supabase usando el puente HTTP compatible sin instalar paquetes extra
+const sql = neon(process.env.DATABASE_URL!);
+export const db = drizzle(sql, { schema });
